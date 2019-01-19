47654aae58842f1deb0ed098435a980e4f6ab9b1
null null <pidof@fuckery.ru>
2019-01-18 19:44:38 -0800

    bot test

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 /* --- LICENCE ---
-* COMPOST is licenced under the GNU GPLv3
+* SHEBA is licenced under the GNU GPLv3
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -14,7 +14,7 @@
 * You should have received a copy of the GNU General Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+// testing the github bot 
 package main
 
 import (
